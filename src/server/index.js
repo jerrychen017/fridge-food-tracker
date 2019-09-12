6fb8e69c2b2415f99c2f7b0465dabe5081993ec2
William Bernardoni <wrbernardoni@gmail.com>
2019-09-12 17:40:38 -0400

    Express server hello world

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express()
+const port = 3000
 
-console.log("Hello world");
 
+app.get('/', function(req, res)
+{
+	console.log("Inbound connection");
+	res.send("Hello world!");
+})
+
+app.listen(port, function()
+{
+	console.log("Server started!");
+})
