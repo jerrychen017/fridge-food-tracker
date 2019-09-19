a7c39aef86a85db6313b17224bc492ee9dd603cd
Darian Low <dlow3@jhu.edu>
2019-09-19 18:16:05 -0400

    firebase vision ML sdk dependencies added

---
+++ src/ReceiptOCR/app/src/main/java/com/example/receiptocr/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.receiptocr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("WOrking directory = " + MainActivity.this.getApplicationInfo().dataDir);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start();
+    }
+
+    public void start(){
+        FirebaseVisionImage image = null;
+
+        try {
+            //System.out.println("WOrking directory = " + System.getProperty("user.home"));
+            File f = new File("receipt2.jpg");
+            if (f != null) {
+                System.out.println(f.toString());
+                return;
+            }
+
+            image = FirebaseVisionImage.fromFilePath(MainActivity.this, Uri.fromFile(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        Task<FirebaseVisionText> result =
+                detector.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                // Task completed successfully
+                                // [START_EXCLUDE]
+                                // [START get_text]
+                                for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+                                    String blockText = block.getText();
+                                    Float blockConfidence = block.getConfidence();
+                                    List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+
+                                    Rect boundingBox = block.getBoundingBox();
+                                    Point[] cornerPoints = block.getCornerPoints();
+                                    String text = block.getText();
+
+                                    for (FirebaseVisionText.Line line: block.getLines()) {
+                                        String lineText = line.getText();
+                                        Float lineConfidence = line.getConfidence();
+                                        List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                                        Point[] lineCornerPoints = line.getCornerPoints();
+                                        Rect lineFrame = line.getBoundingBox();
+                                        for (FirebaseVisionText.Element element: line.getElements()) {
+                                            String elementText = element.getText();
+                                            Float elementConfidence = element.getConfidence();
+                                            List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                                            Point[] elementCornerPoints = element.getCornerPoints();
+                                            Rect elementFrame = element.getBoundingBox();
+                                        }
+                                    }
+                                }
+                                // [END get_text]
+                                // [END_EXCLUDE]
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+    }
+}
