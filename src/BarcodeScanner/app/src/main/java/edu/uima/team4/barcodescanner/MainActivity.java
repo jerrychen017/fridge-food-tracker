c51998cfdb6b2195269529407c1545c9c33af285
Han <hmason9@jhu.edu>
2019-09-25 12:45:04 -0400

    Created (currently empty) app for testing Google's Barcode API

---
+++ src/BarcodeScanner/app/src/main/java/edu/uima/team4/barcodescanner/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uima.team4.barcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
