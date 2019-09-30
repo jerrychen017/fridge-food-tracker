b5570a62909f117869e01629d262107741883ade
William Bernardoni <wrbernardoni@gmail.com>
2019-09-30 14:18:27 -0400

    Removed test database, only fridges now

--- src/server/index.js
+++ src/server/index.js
@@ -8,72 +8,12 @@ app.use(express.json())
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');//new sqlite3.Database('./data/test.db');
 
-db.run('CREATE TABLE IF NOT EXISTS test(id text, desc text)');
-
 var fridgeRoute = require('./fridgeRouter')(db)
 app.use('/fridge', fridgeRoute)
 
-app.post('/', function(req, res)
-{
-	var obj = req.body;
-
-	console.log(obj);
-
-	db.run(`INSERT INTO test(id, desc) VALUES(?, ?)`, [obj.ID, obj.DESC], function(err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    // get the last insert id
-    console.log(`A row has been inserted with rowid ${this.lastID}`);
-  	});
-
-	res.redirect('/');
-})
-
 app.get('/', function(req, res)
 {
-	let sql = `SELECT id identifier,
-                  desc description
-           FROM test
-           ORDER BY identifier`;
- 
-	db.all(sql, [], (err, rows) => {
-	  if (err) {
-	    throw err;
-	  }
-
-	  var output = 
-`<form action="/" method="post">
-ID:<br>
-<input type="text" name="ID"><br>
-Description:<br>
-<input type="text" name="DESC"><br>
-<input type="submit" value="Submit">
-</form>
-
-<table style="width:100%">
-  <tr>
-    <th>id</th>
-    <th>desc</th>
-  </tr>`
-
-	  rows.forEach((row) => {
-	  	output += '<tr>'
-	  	output += '<th>'
-	  	output += row.identifier
-	  	output += '</th>'
-	  	output += '<th>'
-	  	output += row.description
-	  	output += '</th>'
-	    output += '</tr>'
-	  });
-
-	  output += `</table>`
-
-	  res.send(output);
-	});
-
-	console.log("Inbound connection");
+	res.send("Hi there! You aren't supposed to be loading me!")
 })
 
 app.listen(port, function()
