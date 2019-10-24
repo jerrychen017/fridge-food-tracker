64a41c43decc0b3ee5523d3baeed288367d4dee0
Eric Cao <ecao310@gmail.com>
2019-10-24 18:57:50 -0400

    progress bar constants text

---
+++ src/FridgeFoodTracker/app/src/main/java/com/oosegroup/fridgefoodtracker/ProgressBar.java
@@ -0,0 +1,34 @@
+package com.oosegroup.fridgefoodtracker;
+
+public class ProgressBar {
+
+    /** URL to database */
+    public static final String TEN_PERCENT = "..........";
+
+    /** URL to database */
+    public static final String TWENTY_PERCENT = "/........";
+
+    /** URL to database */
+    public static final String THIRTY_PERCENT = "//.......";
+
+    /** URL to database */
+    public static final String FORTY_PERCENT = "///......";
+
+    /** URL to database */
+    public static final String FIFTY_PERCENT = "////.....";
+
+    /** URL to database */
+    public static final String SIXTY_PERCENT = "/////....";
+
+    /** URL to database */
+    public static final String SEVENTY_PERCENT = "//////...";
+
+    /** URL to database */
+    public static final String EIGHTY_PERCENT = "///////..";
+
+    /** URL to database */
+    public static final String NINETY_PERCENT = "////////.";
+
+    /** URL to database */
+    public static final String EXPIRED = "/////////";
+}
