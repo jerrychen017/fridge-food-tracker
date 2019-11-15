b4be1d0a1fe07e095e6630a10c7f4698e4c4fade
rbx999 <rxu22@jhu.edu>
2019-11-14 19:03:57 -0500

    converting listview into expandiblelistview

--- src/FridgeFoodTracker/app/src/main/java/com/oosegroup/fridgefoodtracker/models/ItemListViewAdapter.java
+++ src/FridgeFoodTracker/app/src/main/java/com/oosegroup/fridgefoodtracker/models/ItemListViewAdapter.java
@@ -1,62 +1,124 @@
 package com.oosegroup.fridgefoodtracker.models;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.oosegroup.fridgefoodtracker.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class ItemListViewAdapter extends ArrayAdapter {
-    private final Activity context;
-    private final String[] descriptionArray;
-    private final String[] progressBarArray;
-    private final Integer[] itemIDArray;
+public class ItemListViewAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    //private final String[] descriptionArray;
+    //private final String[] progressBarArray;
+    //private final Integer[] itemIDArray;
+    private List<String> descriptionList;
+    private List<String> progressBarList;
+    private List<Integer> itemIDList;
+    private HashMap<String, List<String>> detailsMap;
     /*
     private Button expandButton;
     private Button deleteButton;
     private String description;
     private ProgressBar progressBar; */
 
-    public ItemListViewAdapter(Activity context, Fridge fridge) {
-        super(context, R.layout.items, fridge.getContent().getItems());
+    public ItemListViewAdapter(Context context, List<String> description, List<String> progressBar, List<Integer> itemID, HashMap<String, List<String>> details) {
 
         this.context = context;
+        this.descriptionList = description;
+        this.progressBarList = progressBar;
+        this.itemIDList = itemID;
+        this.detailsMap = details;
 
-        this.descriptionArray = new String[fridge.getContent().getItems().size() * 2];
+        /*this.descriptionArray = new String[fridge.getContent().getItems().size() * 2];
         this.progressBarArray = new String[fridge.getContent().getItems().size() * 2];
         this.itemIDArray = new Integer[fridge.getContent().getItems().size() * 2];
         for (int i = 0; i < fridge.getContent().getItems().size(); ++i) {
             this.descriptionArray[i] = fridge.getContent().getItems().get(i).getDescription();
             this.progressBarArray[i] = ProgressBar.getView(fridge.getContent().getItems().get(i));
             this.itemIDArray[i] = fridge.getContent().getItems().get(i).getId();
-        }
+        }*/
 
-        /*
-        List<String> progressBars = new ArrayList<>();
-        for (Item item : fridge.getContent().getItems()) {
-            progressBars.add(ProgressBar.getView(item));
-        }
-        this.progressBarArray = (String[]) progressBars.toArray();
 
-        List<Integer> itemIDs = new ArrayList<>();
-        for (Item item : fridge.getContent().getItems()) {
-            itemIDs.add(item.getId());
-        }
-        this.itemIDArray = (Integer[]) progressBars.toArray(); */
-        /*
-        this.expandButton = expandButton;
-        this.deleteButton = deleteButton;
-        this.description = description;
-        this.progressBar = progressBar; */
+    }
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.detailsMap.get(this.descriptionList.get(listPosition)).get(expandedListPosition);
     }
 
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.items_child, null);
+        }
+        TextView expandedListTextView = (TextView) convertView.findViewById(R.id.inpDate);
+        expandedListTextView.setText(expandedListText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.detailsMap.get(this.descriptionList.get(listPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.descriptionList.get(listPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.descriptionList.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.items, null);
+        }
+        TextView listTitleTextView = (TextView) convertView.findViewById(R.id.list_item_string);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
+    }
+
+   /* @Override
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.items, null, true);
@@ -73,5 +135,5 @@ public class ItemListViewAdapter extends ArrayAdapter {
         progressBar.setText(this.progressBarArray[position]);
 
         return rowView;
-    }
+    }*/
 }
